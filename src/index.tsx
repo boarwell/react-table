b19c5b77008d6a096846ebdc7fe541ed968f6425
gorimori <34891695+gorimori@users.noreply.github.com>
2020-07-20 01:21:29 +0900

    sort by click

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,8 @@ const data: SampleType[] = [
   { name: "hoge", age: 12 },
   { name: "fuga", age: 23 },
   { name: "とっても長い名前", age: 23 },
+  { name: "bob", age: 9 },
+  { name: "alice", age: 45 },
 ];
 
 /**
@@ -37,6 +39,10 @@ type Columns<T> = {
      * 列の幅。指定しなければautoに
      */
     width?: string;
+    /**
+     * ソート関数。Array.prototype.sort()の引数になる
+     */
+    compareFunction?: (a: T[K], b: T[K]) => number;
   };
 };
 
@@ -51,6 +57,7 @@ const columns: Columns<SampleType> = {
   age: {
     label: "年齢",
     mapper: (v) => `${v}歳`,
+    compareFunction: (a, b) => a - b,
   },
 };
 
@@ -65,16 +72,91 @@ type Prop<T> = {
 };
 
 /**
+ * 表の状態
+ */
+type State<T> = {
+  sortedBy: keyof T | null;
+  sortReversed: boolean;
+  data: T[];
+};
+
+type Action<T, K extends keyof T = keyof T> =
+  | {
+      type: "sortBy";
+      payload: {
+        key: K;
+        compareFunction?: (a: T[K], b: T[K]) => number;
+      };
+    }
+  | {
+      type: "toggleSortOrder";
+    };
+
+function reducer<T>(state: State<T>, action: Action<T>): State<T> {
+  switch (action.type) {
+    case "sortBy": {
+      const { key, compareFunction } = action.payload;
+      const copy = [...state.data];
+      const sorted = [
+        ...copy.sort((a, b) => {
+          if (compareFunction) {
+            return compareFunction(a[key], b[key]);
+          }
+
+          if (a[key] < b[key]) {
+            return -1;
+          }
+
+          if (a[key] === b[key]) {
+            return 0;
+          }
+
+          return 1;
+        }),
+      ];
+
+      return {
+        ...state,
+        sortedBy: key,
+        sortReversed: false,
+        data: sorted,
+      };
+    }
+
+    case "toggleSortOrder":
+      {
+        const copy = [...state.data];
+
+        return {
+          ...state,
+          sortReversed: !state.sortReversed,
+          data: [...copy.reverse()],
+        };
+      }
+
+      return state;
+  }
+}
+
+/**
  * Tableコンポーネント
  */
 function Table<T>(prop: React.PropsWithChildren<Prop<T>>) {
-  const [data, setData] = React.useState(prop.data);
-  const [sortedBy, setSortedBy] = React.useState<keyof T | null>(null);
-  const [sortDirection, setSortDirection] = React.useState<"ASC" | "DESC">(
-    "ASC"
+  const [state, dispatch] = React.useReducer(
+    function (state: State<T>, action: Action<T>) {
+      // TODO: 型推論の都合でこうなってしまった。改善したい
+      return reducer(state, action);
+    },
+    {
+      sortedBy: null,
+      sortReversed: false,
+      data: prop.data,
+    }
   );
 
-  const colNames = Object.keys(prop.data[0]) as (keyof T)[];
+  const { data, sortedBy, sortReversed } = state;
+
+  const colNames = Object.keys(data[0]) as (keyof T)[];
 
   return (
     <table className="table">
@@ -85,7 +167,20 @@ function Table<T>(prop: React.PropsWithChildren<Prop<T>>) {
               <th
                 className="cell -th"
                 onClick={() => {
-                  setSortedBy(colName);
+                  if (sortedBy === colName) {
+                    dispatch({
+                      type: "toggleSortOrder",
+                    });
+                    return;
+                  }
+
+                  dispatch({
+                    type: "sortBy",
+                    payload: {
+                      key: colName,
+                      compareFunction: prop.columns[colName].compareFunction,
+                    },
+                  });
                 }}
               >
                 {prop.columns[colName].label}
@@ -107,6 +202,14 @@ function Table<T>(prop: React.PropsWithChildren<Prop<T>>) {
           </tr>
         ))}
       </tbody>
+
+      <style jsx>
+        {`
+          .cell.-th {
+            cursor: pointer;
+          }
+        `}
+      </style>
     </table>
   );
 }
