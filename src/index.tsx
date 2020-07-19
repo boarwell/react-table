e16cb99ec6dbb353d0ff038542124272001f88e2
gorimori <34891695+gorimori@users.noreply.github.com>
2020-07-19 23:17:10 +0900

    add comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+/**
+ * 1行に必要なデータ型
+ */
 type SampleType = {
   name: string;
   age: number;
 };
 
+/**
+ * サンプルのデータ
+ */
 const data: SampleType[] = [
   { name: "hoge", age: 12 },
   { name: "fuga", age: 23 },
 ];
 
+/**
+ * 表の列の設定
+ */
 type Columns<T> = {
   [K in keyof T]: {
+    /** theadのth相当のテキスト */
     label: string;
+    /**
+     * 列のデータを表示用に変換する関数
+     *
+     * booleanをoxで表示するなど
+     */
     mapper?: (value: T[K]) => React.ReactChild;
+    /**
+     * 列の幅。指定しなければautoに
+     */
     width?: string;
   };
 };
 
+/**
+ * サンプルの列設定
+ */
 const columns: Columns<SampleType> = {
   name: {
     label: "名前",
@@ -30,11 +51,19 @@ const columns: Columns<SampleType> = {
   },
 };
 
+/**
+ * TableコンポーネントのProp
+ */
 type Prop<T> = {
+  /** 列の表示設定 */
   columns: Columns<T>;
+  /** 実際に表示するデータ（Tが1行になる） */
   data: T[];
 };
 
+/**
+ * Tableコンポーネント
+ */
 function Table<T>(prop: React.PropsWithChildren<Prop<T>>) {
   const colNames = Object.keys(prop.data[0]) as (keyof T)[];
 
